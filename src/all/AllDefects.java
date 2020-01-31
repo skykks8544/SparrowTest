d31db609006437597b0812b35a7a4fecdd23f8f0
skykks8544 <skykks8544@gmail.com>
2020-01-31 17:41:23 +0900

    test

--- src/all/AllDefects.java
+++ src/all/AllDefects.java
@@ -59,7 +59,12 @@ public class AllDefects extends HttpServlet {
         String x = retNull();
         String y = "zxcv";
         String z = "z";
-     
+        if(x!=null){
+        	x=x.toString();
+        }
+        else{
+        	System.out.println("error");
+        }
         x = x.toString();
         y = y.toString();
         z.toString(); // BUG
